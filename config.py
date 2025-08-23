bedbb7abdd5b5950821aa388f562ee06d9573ccd
strad-dev131 <stradiogamer@gmail.com>
2025-08-23 22:15:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/strad-dev131/Robot",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # ----------------------------------------------------------------
